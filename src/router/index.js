a5a81951dee868310af68cd9a21b78aed22acf9e
dhwang <dhwang@thoughtworks.com>
2019-05-05 09:57:38 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Wdh from '@/components/Wdh'
+import Blog from '@/components/Blog'
+
+Vue.use(Router)
+
+const routers = [
+  {
+    path: '/blog',
+    name: 'blog',
+    component: Blog
+  },
+  {
+    path: '/',
+    component: HelloWorld
+  },
+  {
+    path: '/wdh',
+    component: Wdh
+  },
+]
+export default routers
